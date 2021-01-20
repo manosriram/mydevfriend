ef2433788a2174bfbb65616e0a3563739cd1e973
Mano Sriram <mano.sriram0@gmail.com>
2021-01-20 13:21:52 +0530

    Update Language Preference badges

--- client/src/Components/Signup.jsx
+++ client/src/Components/Signup.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Form, Formik } from "formik";
 import {
+    CrossIcon,
+    Badge,
     Pane,
     Autocomplete,
     Heading,
@@ -922,10 +924,6 @@ function Signup(props) {
                         <Autocomplete
                             id="lang"
                             title="Languages"
-                            onSelect={changedItem => {
-                                if (!selectedLanguages.includes(changedItem))
-                                    updateLanguages(changedItem);
-                            }}
                             items={languages}
                         >
                             {props => {
@@ -935,12 +933,28 @@ function Signup(props) {
                                     inputValue
                                 } = props;
                                 return (
-                                    <TextInput
-                                        placeholder="Languages (enter to add)"
-                                        value={inputValue}
-                                        ref={getRef}
-                                        {...getInputProps()}
-                                    />
+                                    <>
+                                        <TextInput
+                                            id="inp-lang"
+                                            placeholder="Languages (enter to add)"
+                                            value={inputValue}
+                                            ref={getRef}
+                                            {...getInputProps()}
+                                        />
+                                        {"  "}
+                                        <Button
+                                            onClick={() => {
+                                                if (
+                                                    !selectedLanguages.includes(
+                                                        inputValue
+                                                    )
+                                                )
+                                                    updateLanguages(inputValue);
+                                            }}
+                                        >
+                                            Add
+                                        </Button>
+                                    </>
                                 );
                             }}
                         </Autocomplete>
@@ -950,7 +964,38 @@ function Signup(props) {
                             {selectedLanguages.map(lang => {
                                 return (
                                     <>
-                                        <Heading size={100}>{lang}</Heading>
+                                        <Badge
+                                            id="badge"
+                                            color="neutral"
+                                            isSolid
+                                            marginRight={10}
+                                        >
+                                            {lang}
+                                            <svg
+                                                id="close"
+                                                width="15"
+                                                height="15"
+                                                viewBox="0 0 15 15"
+                                                fill="none"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                onClick={e => {
+                                                    let newLanguageList = selectedLanguages;
+                                                    newLanguageList = newLanguageList.filter(
+                                                        langg => langg !== lang
+                                                    );
+                                                    setSelectedLanguages(
+                                                        newLanguageList
+                                                    );
+                                                }}
+                                            >
+                                                <path
+                                                    d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+                                                    fill="currentColor"
+                                                    fill-rule="evenodd"
+                                                    clip-rule="evenodd"
+                                                ></path>
+                                            </svg>
+                                        </Badge>
                                     </>
                                 );
                             })}
